0e90a012190794b81a3809a6fe01dfba5d003447
Fn <fn.nk.2002@gmail.com>
2019-06-21 15:49:03 +0900

    結果の種類を増やした

--- GitTest.py
+++ GitTest.py
@@ -2,3 +2,11 @@ input("名前を入力してください＞＞")
 num = int(random.random()*10)
 if(num==0):
     print("大吉")
+elif(num>=1 and num<=5):
+    print("中吉")
+elif(num>=6 and num<=8):
+    print("小吉")
+elif(num==9):
+    print("凶")
+elif(num==10):
+    print("大凶")
